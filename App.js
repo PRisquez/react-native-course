3685ac00cd2877fddfb6e52ece4eb1aec5587be6
PRisquez <prisquez@emergya.com>
2021-11-19 13:40:47 +0100

    Project finished

--- App.js
+++ App.js
@@ -1,21 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Alert,
+  Platform
+} from "react-native";
+import image from "./assets/red-diamond.png";
+import * as ImagePicker from "expo-image-picker";
+import * as Sharing from "expo-sharing";
+import uploadToAnonymousFilesAsync from "anonymous-files";
 
-export default function App() {
+const App = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionREsult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (permissionREsult.granted === false) {
+      alert("Permission to access camera is required");
+      return;
+    }
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    if (Platform.OS ==='web') {
+      const remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
+      console.log(remoteUri)
+    }else{
+      setSelectedImage({ localUri: pickerResult.uri });
+    }
+  };
+
+  const openShareDialog = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert("Sharing is not available on you platform");
+      return;
+    }
+    await Sharing.shareAsync(selectedImage.localUri);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.cotainer}>
+      <Text style={styles.title}>Pick an image</Text>
+      <TouchableOpacity onPress={openImagePickerAsync}>
+        <Image
+          source={{
+            uri:
+              selectedImage !== null
+                ? selectedImage.localUri
+                : "https://picsum.photos/200",
+          }}
+          style={styles.image}
+        />
+      </TouchableOpacity>
+      {selectedImage ? (
+        <TouchableOpacity style={styles.button} onPress={openShareDialog}>
+          <Text style={styles.buttonText}>Share this image</Text>
+        </TouchableOpacity>
+      ) : (
+        <View />
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  cotainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#292929",
   },
+  title: { fontSize: 30, color: "#fff" },
+  image: { height: 200, width: 200, borderRadius: 100, resizeMode: "contain" },
+  button: { backgroundColor: "blue", padding: 7, marginTop: 10 },
+  buttonText: { color: "#fff", fontSize: 20 },
 });
+
+export default App;
